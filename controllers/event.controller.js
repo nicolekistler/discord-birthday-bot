9406105a3a17954b38120d60db8d4e66b667a20b
nicolerockower <njrockower@gmail.com>
2019-12-29 18:35:23 -0500

    Added wish functionality

--- controllers/event.controller.js
+++ controllers/event.controller.js
@@ -2,8 +2,43 @@ const birthdayModel = require('../models/birthday.model');
 const Birthday      = new birthdayModel();
 
 const config = require('../config.json');
+const moment = require('moment');
 
 class Event {
+	/* When send wishes is triggered, send bday wishes */
+	sendWishes(client) {
+		const currentMonth = Number(moment().format('MM'));
+		const currentDay   = Number(moment().format('DD'));
+
+		Birthday.scan({
+			ProjectionExpression: '#bm, #bd, #i, #t',
+			FilterExpression: '#bm = :current_month and #bd = :current_day',
+			ExpressionAttributeNames: {
+				'#bm': 'birth_month',
+				'#bd': 'birth_day',
+				'#i': 'member_id',
+				'#t': 'member_tag'
+			},
+			ExpressionAttributeValues: {
+				':current_month': currentMonth,
+				':current_day': currentDay
+			}
+		}).then(data => {
+			const bdays = [];
+
+			data.Items.forEach(item => {
+				bdays.push(`<@${item.member_id}>`);
+			});
+
+			if(bdays.length) {
+				client
+					.channels
+					.get(config.wishes_channel_id)
+					.send(`${bdays.join(' ')}, happy birthday 🎉!!!`);
+			}
+		});
+	}
+
 	/* When the bot first joins the server, create table */
 	onBotJoin(client) {
 		this.createTable();
